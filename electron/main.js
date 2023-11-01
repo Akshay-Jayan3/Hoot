9fdb8e6cd9604488f57a7cb77a3ea584b07dbea9
Akshay_jayan <akshayjayan321@gmail.com>
2023-11-01 14:12:08 +0530

    Revert "add models"
    This reverts commit 6ea342056e134b513eb0caba88fce865a6e46975.

--- electron/main.js
+++ electron/main.js
@@ -1,10 +1,7 @@
 const { app, BrowserWindow, dialog, ipcMain } = require("electron");
 const isDev = require("electron-is-dev");
+const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
-const dbFunctions = require('./dbFunctions'); // Import the MusicMetadata model
-
-
-
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -17,72 +14,61 @@ function createWindow() {
       webSecurity: false,
     },
   });
-  ipcMain.on('getMusicMetadata', async (event) => {
-    try {
-      const metadata = await dbFunctions.getMusicMetadata();
-      event.reply('musicMetadata', metadata);
-    } catch (error) {
-      event.reply('musicMetadata', []);
+  const dbPath = path.join(__dirname, '../db', 'music.sqlite');
+  console.log(dbPath)
+  let db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message);
+      throw err;
+    } else {
+      console.log("Connected to the SQLite database.");
+      db.run(
+        `CREATE TABLE IF NOT EXISTS metadata (
+          id INTEGER PRIMARY KEY,
+          title TEXT,
+          artist TEXT,
+          album TEXT,
+          filePath TEXT
+        )`,
+        (err) => {
+          if (err) {
+            console.log("Table creation error: ", err);
+          } else {
+            console.log("Table created successfully.");
+          }
+        }
+      );
     }
+    // const newData = {
+    //   title: 'Sample Song',
+    //   artist: 'Sample Artist',
+    //   album: 'Sample Album',
+    //   filePath: '/path/to/sample-song.mp3', // Replace with the actual file path
+    // };
+    
+    // // Insert the data into the 'metadata' table
+    // db.run(
+    //   'INSERT INTO metadata (title, artist, album, filePath) VALUES (?, ?, ?, ?)',
+    //   [newData.title, newData.artist, newData.album, newData.filePath],
+    //   (err) => {
+    //     if (err) {
+    //       console.error('Error inserting data:', err.message);
+    //     } else {
+    //       console.log('Data inserted successfully.');
+    //     }
+    //   }
+    // );
+    
+    
+    db.all("SELECT * FROM metadata", (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('Data from the "metadata" table:', rows);
+      }
+    });
   });
-
-
-
-  // const dbPath = path.join(__dirname, '../db', 'music.sqlite');
-  // console.log(dbPath)
-  // let db = new sqlite3.Database(dbPath, (err) => {
-  //   if (err) {
-  //     // Cannot open database
-  //     console.error(err.message);
-  //     throw err;
-  //   } else {
-  //     console.log("Connected to the SQLite database.");
-  //     db.run(
-  //       `CREATE TABLE IF NOT EXISTS metadata (
-  //         id INTEGER PRIMARY KEY,
-  //         title TEXT,
-  //         artist TEXT,
-  //         album TEXT,
-  //         filePath TEXT
-  //       )`,
-  //       (err) => {
-  //         if (err) {
-  //           console.log("Table creation error: ", err);
-  //         } else {
-  //           console.log("Table created successfully.");
-  //         }
-  //       }
-  //     );
-  //   }
-  //   // const newData = {
-  //   //   title: 'Sample Song',
-  //   //   artist: 'Sample Artist',
-  //   //   album: 'Sample Album',
-  //   //   filePath: '/path/to/sample-song.mp3', // Replace with the actual file path
-  //   // };
-    
-  //   // // Insert the data into the 'metadata' table
-  //   // db.run(
-  //   //   'INSERT INTO metadata (title, artist, album, filePath) VALUES (?, ?, ?, ?)',
-  //   //   [newData.title, newData.artist, newData.album, newData.filePath],
-  //   //   (err) => {
-  //   //     if (err) {
-  //   //       console.error('Error inserting data:', err.message);
-  //   //     } else {
-  //   //       console.log('Data inserted successfully.');
-  //   //     }
-  //   //   }
-  //   // );
-    
-    
-  //   db.all("SELECT * FROM metadata", (err, rows) => {
-  //     if (err) {
-  //       console.error(err.message);
-  //     } else {
-  //       console.log('Data from the "metadata" table:', rows);
-  //     }
-  //   });
-  // });
 
   if (isDev) {
     win.loadURL("http://localhost:3000"); // When in development mode
