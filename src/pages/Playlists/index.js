881235e753ca66639da860876348c5182f5d27f7
Akshay_Jayan <akshay.j@thinkpalm.com>
2023-12-12 15:24:55 +0530

    add context

--- src/pages/Playlists/index.js
+++ src/pages/Playlists/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect ,useContext} from "react";
 import Header from "../../components/Header";
 import Search from "../../components/Search";
 import AudioPlayer from "../../components/AudioPlayer";
