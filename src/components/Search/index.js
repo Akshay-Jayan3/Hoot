9f67a962de9ba7174faff2de24252c7eb5d95ec1
Akshay_Jayan <akshay.j@thinkpalm.com>
2023-12-14 10:05:17 +0530

    add seach icon

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { useTheme } from "../../context/ThemeContext";
 
-const Search = ({ showback, HandleBack, value, onChange ,placeholder}) => {
+const Search = ({ showback, HandleBack, value, onChange, placeholder }) => {
   const { theme } = useTheme();
   return (
-    <div className={styles.SearchBar}  style={{ background: theme.background}}>
+    <div className={styles.SearchBar} style={{ background: theme.background }}>
       {showback && (
         <button onClick={HandleBack}>
           <ChevronLeftOutlinedIcon fontSize="large" />
         </button>
       )}
-      <input type="search" placeholder={placeholder} value={value} onChange={(e)=>onChange(e.target.value)}/>
+      <div className={styles.inputContainer}>
+      <label htmlFor="search">
+        <SearchRoundedIcon className={styles.searchIcon}/>
+        <input
+          id="search"
+          type="search"
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          className={styles.search}
+        ></input>
+      </label>
+      </div>
+     
     </div>
   );
 };
